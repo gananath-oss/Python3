e6cd5a04a72ddb6d9007a9e6da07990d06dc21dd
gananath-oss <56883476+gananath-oss@users.noreply.github.com>
2022-10-17 03:41:17 -0700

    My first python project

---
+++ projects01.py
@@ -0,0 +1,44 @@
+#dicestimulater
+
+#1. Get random numbers
+
+import random
+
+number = random.randint(1,6)
+
+if number == 1:
+    print('----------')
+    print('|        |')
+    print('|    0   |')
+    print('|        |')
+    print('----------')
+elif number == 2:
+    print('----------')
+    print('|        |')
+    print('|  0   0 |')
+    print('|        |')
+    print('----------')
+elif number == 3:
+        print('----------')
+        print('|    0   |')
+        print('|    0   |')
+        print('|    0   |')
+        print('----------')
+elif number == 4:
+    print('----------')
+    print('|  0  0  |')
+    print('|        |')
+    print('|  0  0  |')
+    print('----------')
+elif number == 5:
+        print('----------')
+        print('|  0   0 |')
+        print('|    0   |')
+        print('|  0   0 |')
+        print('----------')
+else:
+        print('----------')
+        print('| 0   0  |')
+        print('| 0   0  |')
+        print('| 0   0  |')
+        print('----------')
